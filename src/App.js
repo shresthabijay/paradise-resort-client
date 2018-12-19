8773f4bf48583685a1c7a0288657b68a6a40c22b
shresthabijay <sbijay777@gmail.com>
2018-12-19 10:32:52 +0545

    project initiation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import khalti from "khalti-web"
+
+let config = {
+  // replace this key with yours
+  "publicKey": "test_public_key_a98c3a80b7ba4c7095627a4e7a1c89dc",
+  "productIdentity": "12345",
+  "productName": "Drogon",
+  "productUrl": "http://gameofthrones.com/buy/Dragons",
+  "amount":2000,
+  "eventHandler": {
+      onSuccess (payload) {
+          // hit merchant api for initiating verfication
+          console.log(payload);
+      },
+      // onError handler is optional
+      onError (error) {
+          // handle errors
+          console.log(error);
+      },
+      onClose () {
+          console.log('widget is closing');
+      }
+  }
+};
+
+let checkout = new khalti(config);
+let btn = document.getElementById("payment-button");
+
+checkout.show()
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        
       </div>
     );
   }
