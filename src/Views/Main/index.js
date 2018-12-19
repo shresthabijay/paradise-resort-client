62c429f5c59cdc72abaab2482e5c4eb5e31cdd1d
shresthabijay <sbijay777@gmail.com>
2018-12-19 16:44:06 +0545

    folder structure made

--- src/Views/Main/index.js
+++ src/Views/Main/index.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react'
+import {Switch,Route} from "react-router-dom"
+import HomeSection from './Home';
+import EventSection from './Event';
+import BookingSection from './Booking';
+import LocateSection from './LocateUs';
+import Page404 from '../Page404';
+
 
 export default class Main extends Component {
   render() {
     return (
       <div>
-          Main
+          <Switch>
+            <Route path="/home" component={HomeSection}/>
+            <Route path="/events" component={EventSection}/>
+            <Route path="/booking" component={BookingSection}/>
+            <Route path="/locateus" component={LocateSection}/>
+            <Route path="*" component={Page404}/>
+
+          </Switch>
       </div>
     )
   }
